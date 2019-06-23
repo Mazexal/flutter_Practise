920fca4e88fb5594156b3434f2424ac294abf962
Mazexal <1023520044@qq.com>
2019-06-23 21:09:17 +0800

    IO以及网络IO

--- lib/fileIOAndWebIO/DownloadWithChunks.dart
+++ lib/fileIOAndWebIO/DownloadWithChunks.dart
@@ -73,6 +73,21 @@ class DownLoadUtils{
       await mergeTempFiles(chunk);
     }
   }
+
+
+  _request() async{
+    //建立连接
+    var socket=await Socket.connect("baidu.com", 80);
+    //根据http协议，发送请求头
+    socket.writeln("GET / HTTP/1.1");
+    socket.writeln("Host:baidu.com");
+    socket.writeln("Connection:close");
+    socket.writeln();
+    await socket.flush(); //发送
+    //读取返回内容
+    var _response =await socket.transform(utf8.decoder).join();
+    await socket.close();
+  }
 }
 
 //main() async {
---
+++ lib/fileIOAndWebIO/JsonTest.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+class JsonTest{
+
+  void main(){
+//一个JSON格式的用户列表字符串
+    String jsonStr='[{"name":"Jack"},{"name":"Rose"}]';
+//将JSON字符串转为Dart对象(此处是List)
+    List items=json.decode(jsonStr);
+//输出第一个用户的姓名
+    print(items[0]["name"]);
+
+
+  }
+
+}
